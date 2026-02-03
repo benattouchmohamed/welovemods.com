98215cb5709fa139644dfe6a2ad340607bed342d
benattouchmohamed <benattouchmohamed7@gmail.com>
2026-02-03 22:42:54 +0000

    Resolved merge conflicts between local master and remote main

--- src/services/offerService.ts
+++ src/services/offerService.ts
@@ -129,7 +129,7 @@ export const fetchOffers = async (): Promise<Offer[]> => {
     // 5. ULTIMATE FALLBACK: Best 4 general offers
     return mapped
       .sort((a, b) => b.epc - a.epc)
-      .slice(0, 1);
+      .slice(0,3 );
 
   } catch (err) {
     console.error("❌ Offer Fetch Error", err);
